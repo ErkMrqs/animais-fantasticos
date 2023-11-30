a09f6530afe30c7ba975bfcd8d055ffd1a573a45
Erick Marques <erickmventura25@gmail.com>
2023-11-30 09:06:44 -0300

    Fix: Scroll Anima Refatorado

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import ScrollAnima from "./modules/scroll-anima.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 
 // Scroll
@@ -37,6 +37,8 @@ modal.init();
 // Tooltip
 const tooltip = new Tooltip("[data-tooltip]");
 tooltip.init();
+// Scroll Anima
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 
 // Fetch Animais
 fetchAnimais("../../Animaisapi.json", ".numeros-grid");
@@ -44,4 +46,3 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchBitcoin();
-initAnimacaoScroll();
